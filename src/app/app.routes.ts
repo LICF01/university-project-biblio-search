e196bb0f819583aad12f448d908b8abcab026fa8
LICF01 <lucascubilla@gmail.com>
2024-06-19 22:35:03 -0400

    Delete old components and routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,22 +1,11 @@
 import { Routes } from '@angular/router';
-import { BookSearchComponent } from './features/book-search/book-search.component';
-import { BookManagerComponent } from './features/book-manager/book-manager.component';
 import { FacultyComponent } from './features/faculty/faculty.component';
 
 export const routes: Routes = [
   {
     path: '',
-    component: BookSearchComponent,
-  },
-  {
-    path: 'books/search',
-    title: 'Search',
-    component: BookSearchComponent,
-  },
-  {
-    path: 'books/manager',
-    title: 'Manage books',
-    component: BookManagerComponent,
+    redirectTo: '/faculty',
+    pathMatch: 'full',
   },
   {
     path: 'faculty',
