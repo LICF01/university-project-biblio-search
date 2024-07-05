3150cc67c0e5da8b74a2b277a5aab45cb78975f6
LICF01 <lucascubilla@gmail.com>
2024-07-05 15:47:15 -0400

    Add base components and routes for resources

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { FacultyComponent } from './features/faculty/faculty.component';
 import { SubjectComponent } from './features/subject/subject.component';
+import { ResourcesComponent } from './features/resources/resources.component';
 
 export const routes: Routes = [
   {
@@ -17,5 +18,10 @@ export const routes: Routes = [
     path: 'subject',
     title: 'Administrar Materias',
     component: SubjectComponent,
+  },
+  {
+    path: 'resources',
+    title: 'Administrar Recursos',
+    component: ResourcesComponent,
   },
 ];
